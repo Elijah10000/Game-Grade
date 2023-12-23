12ce2c5358f7bed74449212695d8a103249f4ee6
Elijah2609 <elijahlyndon14@gmail.com>
2023-12-23 00:29:20 +0000

    Initial screens and emulator configuration

---
+++ GameGrade/screens/HomeScreen.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const HomeScreen: React.FC<{ navigation: any }> = ({
+  navigation,
+}): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default HomeScreen;
